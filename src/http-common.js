af002a5a8b298f32ed7e23e0e408c4bc385e933d
홍석용 <dennis.hong@kakaocorp.com>
2022-02-07 12:06:24 +0000

    fix: 수정

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://spring-boot-data-jpa-rutah.run.goorm.io/api",
   headers: {
     "Content-type": "application/json"
   }
